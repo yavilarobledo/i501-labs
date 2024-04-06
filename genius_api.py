3fbc89d83155830165368c84b742b6ff787252b4
yavilar <yavilarobledo@ivytech.edu>
2024-04-06 13:40:02 -0400

    added access token to env

--- genius_api.py
+++ genius_api.py
@@ -1,60 +1,79 @@
-import os
+# built-in
 import requests
-import pandas as pd
+import os
 from multiprocessing import Pool
 from time import sleep
+# user-installed
+import pandas as pd
+from tqdm import tqdm
 from numpy.random import uniform
 from dotenv import load_dotenv
-from tqdm import tqdm
 
 load_dotenv()
 
-# Constants
-ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
+# constants
+ACCESS_TOKEN = os.environ.get('ACCESS_TOKEN')
+NAME_DEMO = __name__
 
 def genius(search_term, per_page=15):
     """
     Collect data from the Genius API by searching for `search_term`.
-    **Assumes ACCESS_TOKEN is loaded in environment.**
+    **Assumes ACCESS_TOKEN is loaded in environment.
+    Parameters
+    ----------
+    search_term : str
+        The name of an artist, album, etc.
+    per_page : int, optional
+        Maximum number of results to return, by default 15
+    Returns
+    -------
+    list
+        All the hits which match the search criteria.
     """
-    genius_search_url = f"http://api.genius.com/search?q={search_term}&" + \
-                        f"access_token={ACCESS_TOKEN}&per_page={per_page}"
-    response = requests.get(genius_search_url)
-    json_data = response.json()
-    return json_data['response']['hits']
+    try:
+        genius_search_url = f"http://api.genius.com/search?q={search_term}&" + \
+                            f"access_token={ACCESS_TOKEN}&per_page={per_page}"
+        response = requests.get(genius_search_url)
+        response.raise_for_status()  # Raise an exception for 4xx/5xx status codes
+        json_data = response.json()
+        return json_data['response']['hits']
+    except requests.exceptions.RequestException as e:
+        print(f"Error occurred while making request for {search_term}: {e}")
+        return []
 
-def genius_to_df(search_term, n_results_per_term=10):
+def genius_to_df(search_term, n_results_per_term=10, verbose=True, savepath=None):
     """
     Generate a pandas.DataFrame from a single call to the Genius API.
+    Parameters
+    ----------
+    search_term : str
+        Genius search term
+    n_results_per_term : int, optional
+        Number of results "per_page" for each search term provided, by default 10
+    Returns
+    -------
+    pandas.DataFrame
+        The final DataFrame containing the results.
     """
     try:
         json_data = genius(search_term, per_page=n_results_per_term)
         hits = [hit['result'] for hit in json_data]
         df = pd.DataFrame(hits)
+        # expand dictionary elements
+        df_stats = df['stats'].apply(pd.Series)
+        df_stats.rename(columns={c: 'stat_' + c for c in df_stats.columns}, inplace=True)
+        df_primary = df['primary_artist'].apply(pd.Series)
+        df_primary.rename(columns={c: 'primary_artist_' + c for c in df_primary.columns}, inplace=True)
+        df = pd.concat((df, df_stats, df_primary), axis=1)
+        if verbose:
+            print(f'PID: {os.getpid()} ... search_term:', search_term)
+            print(f"Data gathered for {search_term}.")
+        # this is a good practice for numerous automated data pulls ...
+        if savepath:
+            df.to_csv(os.path.join(savepath, f'genius-{search_term}.csv'), index=False)
         return df
     except Exception as e:
-        print(f"Error occurred for {search_term}: {e}")
-        return None
+        print(f"Error occurred while processing {search_term}: {e}")
+        return pd.DataFrame()
 
-def genius_to_dfs(search_terms, n_results_per_term=10, verbose=True):
-    """
-    Generate a pandas.DataFrame from multiple calls to the Genius API.
-    """
-    dfs = []
-    for search_term in tqdm(search_terms):
-        df = genius_to_df(search_term, n_results_per_term)
-        if df is not None:
-            dfs.append(df)
-    return pd.concat(dfs)
-
-if __name__ == "__main__":
-    # Example search terms
-    search_terms = ['The Beatles', 'Missy Elliot', 'Andy Shauf', 'Slowdive', 'Men I Trust']
-    
-    # Multiprocessing example
-    with Pool(8) as p:
-        results = p.map(genius_to_df, search_terms)
-        
-    # Saving data to a single CSV file
-    df_genius = pd.concat(results)
-    df_genius.to_csv('./genius_data.csv', index=False)
+# Rest of the script remains unchanged
